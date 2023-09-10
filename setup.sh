f25f84487e99670c7c67c4af8def13bd87b7b8dc
Tanmay Jain <85993243+TanmayDoesAI@users.noreply.github.com>
2023-09-10 10:49:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
+
+# Create the Streamlit credentials file
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "your-email@domain.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
-" > ~/.streamlit/config.toml
+echo "[general]" > ~/.streamlit/credentials.toml
+echo "email = \"tanmay5tj@gmail.com\"" >> ~/.streamlit/credentials.toml
+
+# Create the Streamlit config file
+echo "[server]" > ~/.streamlit/config.toml
+echo "headless = true" >> ~/.streamlit/config.toml
+echo "enableCORS = false" >> ~/.streamlit/config.toml
+echo "port = $PORT" >> ~/.streamlit/config.toml
