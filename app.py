f1d0c1f0cc0e06ed5955f652d0f7045b507f3005
Tanmay Jain <85993243+TanmayDoesAI@users.noreply.github.com>
2023-09-10 23:56:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,10 @@ from streamlit_pandas_profiling import st_profile_report
 if 'running' not in st.session_state:
     st.session_state.running = True
 
-# st.set_option('server.port', 8501)
-
-
 with st.sidebar:
     st.title('Data-chain')
     choice=st.radio('Navigate through the app',['Upload your file','Profiling your columns','Train your model','Download the trained model'])
-    st.info('some information about the app in infobox')
+    st.info('Data-Chain is your hassle-free data analysis companion. Upload your data, get automated insights, and train models without any coding. It's quick, simple, and perfect for non-coders.')
 
 if os.path.exists("data.csv"):
     df=pd.read_csv("data.csv",index_col=None)
@@ -64,7 +61,7 @@ elif choice=='Train your model':
         st.info("These are the model comparisions")
         st.dataframe(compare_df)
         best_model
-        # save_model(best_model,'final_model')
+        save_model(best_model,'final_model')
 
 elif choice=='Download the trained model':
     st.title("Download the model")
